a0f0dbecf881aeb0f40560e4c3b07ba342c16c3a
kgeyer <karel.geyer@nakit.cz>
2026-02-04 10:57:13 +0100

    Rename Projects

---
+++ Eve.Backend/Modules/Users/Users.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entites;
+
+namespace Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUser(int id);
+    }
+}
