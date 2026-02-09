d737c971182d4280b76aae3ea9fca29879523188
kgeyer <karel.geyer@nakit.cz>
2026-02-09 20:10:20 +0100

    Adjustments to main program.

--- Eve.Backend/Eve.API/Program.cs
+++ Eve.Backend/Eve.API/Program.cs
@@ -1,33 +1,52 @@
 using Common.Shared.Interfaces;
 using EmailService;
+using Eve.API.Extensions;
+using Eve.API.Middlewares;
 using Scalar.AspNetCore;
+using Users.Application.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+//*** Manage Secrets ***//
+// Naplnìní ExternalSettings -> User.Application
+builder.Services.Configure<ExternalSettings>(builder.Configuration.GetSection("ABSTRACT_API"));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference(); // Pøidá vizuální rozhraní Scalar
-}
+//*** Rate Limiting ***//
+builder.Services.AddAppRateLimiting(builder.Configuration);
 
-// Email Klient
+//*** Email Klient ***//
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("Email"));
 builder.Services.AddTransient<IEmailService, MailService>();
 
-app.UseHttpsRedirection();
+//*** Middleware ***//
+builder.Services.AddHttpContextAccessor();
 
+//*** DI ***//
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    // UI For Testing API Endpoints
+    app.MapScalarApiReference();
+}
+
+//*** Middleware ***//
+app.UseMiddleware<HeadersMiddleware>();
+app.UseMiddleware<CorrelationIdMiddleware>();
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseRateLimiter();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
